88f9fd74c2ad4ddd41d6495fbe667ce1d11cbbbd
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-04-21 14:32:05 -0300

    perf: input as a form component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -5,23 +5,23 @@ interface InputProps extends ChakraInputProps {
     label: string,
 }
 
-export default function Input({ name, label, ...rest }: InputProps) {
+export function Input({ name, label, ...rest }: InputProps) {
     return (
-        <FormControl>
-            <FormLabel htmlFor={name} >{label}</FormLabel>
-            <ChakraInput
-              name={name}
-              id={name}
-              bg="gray.900"
-              type="email"
-              size="lg"
-              variant="filled"
-              focusBorderColor="pink.700"
-              _hover={{
-                bg:"gray.900"
-              }}
-              {...rest}
-            />
-          </FormControl>
+      <FormControl>
+        <FormLabel htmlFor={name} >{label}</FormLabel>
+        <ChakraInput
+          name={name}
+          id={name}
+          bg="gray.900"
+          type="email"
+          size="lg"
+          variant="filled"
+          focusBorderColor="pink.700"
+          _hover={{
+            bg:"gray.900"
+          }}
+          {...rest}
+        />
+      </FormControl>
     )
 }
