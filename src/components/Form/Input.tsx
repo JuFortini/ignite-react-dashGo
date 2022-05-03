1c2b60cfdeae7aba1808522816e372ad50cc7485
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-05-03 16:43:29 -0300

    perf: validating forms

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,14 +1,18 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
     name: string,
-    label: string,
+    label?: string,
+    error?: FieldError,
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }, ref) => {
     return (
-      <FormControl>
-        <FormLabel htmlFor={name}>{label}</FormLabel>
+      <FormControl isInvalid={!!error}>
+        { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        
         <ChakraInput
           name={name}
           id={name}
@@ -19,8 +23,14 @@ export function Input({ name, label, ...rest }: InputProps) {
           _hover={{
             bg:"gray.900"
           }}
+          ref={ref}
           {...rest}
         />
+        { !!error && (
+          <FormErrorMessage>{error.message}</FormErrorMessage>
+        )}
       </FormControl>
-    )
+    );
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,12 +1,44 @@
 import { Box, Button, Divider, Flex, Heading, HStack, Icon, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiCheckLine, RiCloseLine } from "react-icons/ri";
-import { Input } from "../../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+    name: string,
+    email: string,
+    password: string,
+    password_confirmation: string,
+}
+
+const createUserFormSchema = yup.object().shape({
+    name: yup.string().required("Nome obrigatório"),
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    password: yup.string().required("Senha obrigatória").min(6, "Mínimo de 6 caracteres"),
+    password_confirmation: yup.string().oneOf([
+        yup.ref("password")
+    ], "Senhas precisam ser iguais")
+})
+
 export default function CreateUser() {
+
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(createUserFormSchema),
+    });
+
+    const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        console.log(values);
+    }
+
+    const { errors } = formState;
+
     return (
         <Box>
             <Header />
@@ -14,19 +46,49 @@ export default function CreateUser() {
             <Flex w="100%" maxWidth={1480} mx="auto" px="6" my="6">
                 <Sidebar />
 
-                <Box flex="1" bg="gray.800" borderRadius={8} p={["6", "8"]}>
+                <Box
+                  as="form"
+                  flex="1"
+                  bg="gray.800"
+                  borderRadius={8}
+                  p={["6", "8"]}
+                  onSubmit={handleSubmit(handleCreateUser)}
+                >
                     <Heading size="lg" fontWeight="normal">Criar usuário</Heading> 
 
                     <Divider my="8" borderColor="gray.700" />
 
                     <VStack spacing="8">
                         <SimpleGrid w="100%" spacing={["6", "8"]} minChildWidth="240px">
-                            <Input name="name" label="Nome completo"/>
-                            <Input name="email" type="email" label="E-mail"/>       
+                            <Input
+                              name="name"
+                              label="Nome completo" 
+                              {...register("name")}
+                              error={errors.name}
+                            />
+                            <Input
+                              name="email"
+                              type="email"
+                              label="E-mail" 
+                              {...register("email")}
+                              error={errors.email}
+                            />      
                         </SimpleGrid>
                         <SimpleGrid w="100%" spacing={["6", "8"]} minChildWidth="240px">
-                            <Input name="password" type="password" label="Senha"/>
-                            <Input name="password_confirmation" type="password" label="Confirmação da senha"/> 
+                            <Input
+                              name="password"
+                              type="password"
+                              label="Senha"
+                              {...register("password")}
+                              error={errors.password} 
+                            />
+                            <Input
+                              name="password_confirmation"
+                              type="password"
+                              label="Confirmação da senha" 
+                              {...register("password_confirmation")}
+                              error={errors.password_confirmation}
+                            /> 
                         </SimpleGrid>
                     </VStack>  
 
@@ -44,11 +106,12 @@ export default function CreateUser() {
                                 </Button>
                             </Link>
                             <Button
-                              as="a"
+                              type="submit"
                               leftIcon={<Icon as={RiCheckLine} fontSize="20" />}
                               size="sm"
                               fontSize="sm"
                               colorScheme="pink"
+                              isLoading={formState.isSubmitting}
                             >
                                 Salvar
                             </Button>
