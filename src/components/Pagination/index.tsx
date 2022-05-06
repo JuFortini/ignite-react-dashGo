bcb9b4e3630cd7c86f8bf53928e94a8a101384a2
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-05-06 16:10:51 -0300

    feat: functional user list e pagination with React Query

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,72 @@
-import { Box, Button, HStack, Stack } from "@chakra-ui/react";
+import { Box, HStack, Stack, Text } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+    totalCountOfRegisters: number,
+    registersPerPage: number,
+    currentPage: number,
+    onPageChange: (page: number) => void;
+}
+
+function generatePagesArray( from: number, to: number ) {
+    return [... new Array(to - from)].map((_, index) => {
+        return (from + index + 1);
+    }) 
+}
+
+export function Pagination({
+    totalCountOfRegisters,
+    registersPerPage,
+    currentPage,
+    onPageChange,
+}: PaginationProps) {
+
+    const siblingsPage = 1;
+
+    const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+    const previousPages = currentPage > 1
+        ? generatePagesArray(currentPage - 1 - siblingsPage, currentPage - 1)
+        : [];
+    const nextPages = currentPage < lastPage
+        ? generatePagesArray(currentPage, Math.min(currentPage + siblingsPage, lastPage))
+        : [];
+
     return (
         <Stack direction={["column", "row"]} align="center" justify="space-between">
             <Box>
                 <strong>0</strong> - <strong>10</strong> de <strong>50</strong>
             </Box>
             <HStack>
-                <PaginationItem isCurrent number={1}/>
-                <PaginationItem number={2}/>
-                <PaginationItem number={3}/>
-                <PaginationItem number={4}/>
-                <PaginationItem number={5}/>
+
+                { currentPage > siblingsPage + 1 && (
+                    <>
+                        <PaginationItem number={1} onPageChange={onPageChange} />
+                        { currentPage > siblingsPage + 2 && (
+                            <Text color="gray.600" px="1" pt="2">...</Text>
+                        )}
+                    </>
+                )}
+
+                { previousPages.length > 0 && previousPages.map(page => {
+                    return <PaginationItem key={page} number={page} onPageChange={onPageChange} />
+                })}
+                
+                <PaginationItem isCurrent number={currentPage} onPageChange={onPageChange} />
+
+                { nextPages.length > 0 && nextPages.map(page => {
+                    return <PaginationItem key={page} number={page} onPageChange={onPageChange} />
+                })}
+
+                {currentPage < lastPage - siblingsPage && (
+                    <>
+                        { currentPage < lastPage - siblingsPage - 1 && (
+                            <Text color="gray.600" px="1" pt="2">...</Text>
+                        )}
+                        <PaginationItem number={lastPage} onPageChange={onPageChange} />
+                    </>
+                )}
+
             </HStack>
         </Stack>
     );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,22 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Text, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { useState } from "react";
 import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Text, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(currentPage);
+
     const isLargeWindow = useBreakpointValue({
         base: false,
-        large: true,
-    })
+        lg: true,
+    });
 
     return (
         <Box>
@@ -24,7 +29,13 @@ export default function UserList() {
                 <Box flex="1" bg="gray.800" borderRadius={8} p="8">
 
                     <Flex justify="space-between" align="center">
-                        <Heading fontWeight="normal" size="lg">Usuários</Heading>
+                        <Heading fontWeight="normal" size="lg">
+                            Usuários
+
+                            { !isLoading && isFetching &&
+                                <Spinner ml="6" size="sm" color="gray.500" />
+                            }
+                        </Heading>
                         <Link href="/users/create" passHref>
                             <Button
                               as="a"
@@ -38,92 +49,74 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha" my="10">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink"/>
-                                </Th>
-                                <Th>USUÁRIO</Th>
-                                {isLargeWindow && <Th>DATA DE CADASTRO</Th>}
-                                {isLargeWindow && <Th></Th>}
-                            </Tr>
-                        </Thead>
+                    { isLoading ? (
+                        <Flex justify="center">
+                            <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.400"
+                            color="pink.500"
+                            size="xl" 
+                            />
+                        </Flex>
+                        ) : error ? (
+                            <Flex justify="center">
+                                <Text color="pink.400" fontSize="lg">Falha ao carregar a lista de usuários</Text>
+                            </Flex>
+                        ) : (
+                            <>
+                                <Table colorScheme="whiteAlpha" my="10">
+                                    <Thead>
+                                        <Tr>
+                                            <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                                <Checkbox colorScheme="pink"/>
+                                            </Th>
+                                            <Th>USUÁRIO</Th>
+                                            {isLargeWindow && <Th>DATA DE CADASTRO</Th>}
+                                            {isLargeWindow && <Th></Th>}
+                                        </Tr>
+                                    </Thead>
 
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="medium">Jucimara Fortini</Text>
-                                        <Text fontSize="sm" color="gray.300">jucimaraafortini@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isLargeWindow && <Td>23 de Abril, 2022</Td>}
-                                {isLargeWindow && 
-                                    <Td>
-                                        <Button
-                                        as="a"
-                                        leftIcon={<Icon as={RiPencilLine} />}
-                                        size="xs"
-                                        fontSize="xs"
-                                        colorScheme="whiteAlpha"
-                                        >
-                                            Editar
-                                        </Button>
-                                    </Td>}
-                            </Tr>
+                                    <Tbody>
+                                        {data.users.map(user => {
+                                            return (
+                                                <Tr key={user.id}>
+                                                    <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
+                                                    <Td>
+                                                        <Box>
+                                                            <Text fontWeight="medium">{user.name}</Text>
+                                                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                        </Box>
+                                                    </Td>
+                                                    {isLargeWindow && <Td>{user.createdAt}</Td>}
+                                                    {isLargeWindow && 
+                                                        <Td>
+                                                            <Button
+                                                            as="a"
+                                                            leftIcon={<Icon as={RiPencilLine} />}
+                                                            size="xs"
+                                                            fontSize="xs"
+                                                            colorScheme="whiteAlpha"
+                                                            >
+                                                                Editar
+                                                            </Button>
+                                                        </Td>}
+                                                </Tr>
+                                            )
+                                        })}
+                                    </Tbody>
 
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="medium">Jucimara Fortini</Text>
-                                        <Text fontSize="sm" color="gray.300">jucimaraafortini@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isLargeWindow && <Td>23 de Abril, 2022</Td>}
-                                {isLargeWindow && 
-                                    <Td>
-                                        <Button
-                                        as="a"
-                                        leftIcon={<Icon as={RiPencilLine} />}
-                                        size="xs"
-                                        fontSize="xs"
-                                        colorScheme="whiteAlpha"
-                                        >
-                                            Editar
-                                        </Button>
-                                    </Td>}
-                            </Tr>
+                                </Table>
 
-                            <Tr>
-                                <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="medium">Jucimara Fortini</Text>
-                                        <Text fontSize="sm" color="gray.300">jucimaraafortini@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isLargeWindow && <Td>23 de Abril, 2022</Td>}
-                                {isLargeWindow && 
-                                    <Td>
-                                        <Button
-                                        as="a"
-                                        leftIcon={<Icon as={RiPencilLine} />}
-                                        size="xs"
-                                        fontSize="xs"
-                                        colorScheme="whiteAlpha"
-                                        >
-                                            Editar
-                                        </Button>
-                                    </Td>}
-                            </Tr>
-                        </Tbody>
-
-                    </Table>
-
-                    <Pagination />
+                                <Pagination
+                                  registersPerPage={10}
+                                  totalCountOfRegisters={data.totalCount}
+                                  currentPage={currentPage}
+                                  onPageChange={setCurrentPage}
+                                />
+                            </>
+                        )
+                    }
 
                 </Box>
 
